2274437b888536d47e668c261c76df2b066ef7a4
aveesp <aveesp@gmail.com>
2023-06-07 22:45:25 +0530

    7 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,31 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import {MatSidenavModule, MatToolbarModule} from '@angular/material';
-import {ModalModule} from 'ngx-bootstrap';
+import {
+  MatButtonModule,
+  MatGridListModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+} from '@angular/material';
+import { ModalModule } from 'ngx-bootstrap';
 
-import {ToolbarComponent} from './toolbar/toolbar.component';
-import {SidenavComponent} from './sidenav/sidenav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
-import {HomeComponent} from './home/home.component';
-import {TaskComponent} from './task/task.component';
-import {PageNotFoundComponent} from './page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -24,7 +35,14 @@ import {PageNotFoundComponent} from './page-not-found.component';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    NgbModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
   ],
   declarations: [
     AppComponent,
@@ -32,11 +50,9 @@ import {PageNotFoundComponent} from './page-not-found.component';
     TaskComponent,
     PageNotFoundComponent,
     ToolbarComponent,
-    SidenavComponent
+    SidenavComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
-
+export class AppModule {}
